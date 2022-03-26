4565b4faa0154072e8d07878284af6be6a3915fa
zshancs <mahmad.zeeshan72@gmail.com>
2022-03-26 11:45:22 +0500

    code code🧑‍💻to docstring book 📑 completed

--- server.py
+++ server.py
@@ -1,18 +1,17 @@
-from copyreg import constructor
 from fastapi import FastAPI
-from server_models import Prompt_Language, IntentAnalysis
+from server_models import Prompt, Prompt_Language, IntentAnalysis
 from templates import (code2nl, fix_bugs, get_api_request_code,
                        get_error_explanation, nl2sql, sql2nl, code2docstring, get_oneliner, code2ut,complete_code)
 
 '''
     code2nl, ☑️
-    fix_bugs,
+    fix_bugs, ☑️
     get_api_request_code,
-    get_error_explanation
+    get_error_explanation, ☑️
     nl2sql,
-    sql2nl,
-    code2docstring,
-    get_oneliner,
+    sql2nl, ☑️
+    code2docstring, ☑️
+    get_oneliner, ☑️
     code2ut,
     complete_code
 '''
@@ -28,17 +27,62 @@ async def FixBugs(data:Prompt_Language):
     print(data)
     return {'status':'ok', 'output':fix_bugs(data.prompt, data.language)}
 
+@app.post('/explain_error')
+async def GetErrorExplanation(data:Prompt):
+    print(data)
+    return {'status':'ok', 'output':get_error_explanation(data.prompt)}
+
+@app.post('/sql2nl')
+async def SQL_to_NL(data:Prompt):
+    print(data)
+    return {'status':'ok', 'output':sql2nl(data.prompt)}
+
+@app.post('/oneliner')
+async def One_Liner(data:Prompt_Language):
+    print(data)
+    return {'status':'ok', 'output':get_oneliner(data.prompt, data.language)}
+
+@app.post('/code2docstring')
+async def Code2DocString(data:Prompt):
+    print(data)
+    return {'status':'ok', 'output':code2docstring(data.prompt)}
+
+@app.post('/code2ut')
+async def Code2DocString(data:Prompt_Language):
+    print(data)
+    return {'status':'ok', 'output':code2ut(data.prompt, data.language)}
+
+
 @app.post('/intent')
 async def get_intent(query:IntentAnalysis):
     query = query.query
-    intent_list = []
+    intent_list = set()
     if query.lower().find('code') != -1:
-        intent_list.append('code2nl')
+        intent_list.add('code2nl')
+
     if query.lower().find('sql') != -1:
-        intent_list.extend(['sql2nl','nl2sql'])
+        intent_list.add('sql2nl')
+        intent_list.add('nl2sql')
+
     if query.lower().find('bugs') != -1:
-        intent_list.extend(['fix_bugs'])
+        intent_list.add('fix_bugs')
+        intent_list.add('get_error_explanation')
+
+    if query.lower().find('error') != -1:
+        intent_list.add('get_error_explanation')
     
+    if query.lower().find('one') != -1:
+        intent_list.add('get_oneliner')
+    if query.lower().find('shorten') != -1:
+        intent_list.add('get_oneliner')
+    
+    
+    if query.lower().find('docstring') != -1:
+        intent_list.add('code2docstring')
+    if query.lower().find('documentation') != -1:
+        intent_list.add('code2docstring')
+
+
     if len(intent_list) == 0:
-        return ['magic']
-    return {'status':'ok', 'output':intent_list}
+        return {'status':'ok', 'output':['magic']}
+    return {'status':'ok', 'output':list(intent_list)}
--- server_models.py
+++ server_models.py
@@ -1,8 +1,12 @@
 from pydantic import BaseModel
 
+class Prompt(BaseModel):
+    prompt:str
 class Prompt_Language(BaseModel):
     prompt:str
     language:str
+
+
 
 class IntentAnalysis(BaseModel):
     query:str

3119b17d70935639f9bf9c2473f3036c377b28f4
Qasim Khan <straightouttadetroit.qk@gmail.com>
2022-03-30 12:21:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -103,4 +103,4 @@ async def get_intent(query: IntentAnalysis):
 
   
   if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=80, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=80, log_level="info")
